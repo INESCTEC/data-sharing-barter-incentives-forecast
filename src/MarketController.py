8b9fa4a7c01ef606355d76246745f7bd56811a3d
jrsa <jose.r.andrade@inesctec.pt>
2023-01-20 19:20:00 +0000

    Improved debugging

--- src/MarketController.py
+++ src/MarketController.py
@@ -336,6 +336,8 @@ class MarketController:
         # #################################
         # Fetch session info:
         session_info = get_session_data(self.api)
+        logger.debug(f"\nSession data:"
+                     f"\n{json.dumps(session_info, indent=3)}")
         session_data = session_info["session_data"]
         bids_per_resource = session_info["bids_per_resource"]
         users_resources = session_info["users_resources"]
