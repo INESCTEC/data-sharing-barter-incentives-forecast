1a3d651d1d48a3ea3b3673512eab068d6ca8d06e
José Andrade <jrsa2012@gmail.com>
2025-10-03 10:36:59 +0100

    updated market price limits

--- conf/settings.py
+++ conf/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
 class FirstSessionConfigs:
     session_number = 1
     b_min = 1         # Minimum market price
-    b_max = 10          # Maximum market price
+    b_max = 20          # Maximum market price
     n_price_steps = 20  # Number of price steps
     delta = 0.05        # Learning rate for price updates
     possible_p = np.linspace(start=b_min,
